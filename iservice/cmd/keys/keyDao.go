4f57f673f096d2567ae130e81814cc50db4f31e0
chengwenxi <vincent.ch.cn@gmail.com>
2020-02-27 17:57:13 +0800

    add key store

---
+++ iservice/cmd/keys/keyDao.go
@@ -0,0 +1,73 @@
+package keys
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/irisnet/irishub-sdk-go/types"
+)
+
+type KeyDAO struct {
+	Path string // the Path to store keys
+}
+
+func NewKeyDAO(path string) KeyDAO {
+	return KeyDAO{
+		Path: path,
+	}
+}
+
+func (dao KeyDAO) Write(name string, store types.Store) error {
+	bz, err := json.Marshal(store)
+	if err != nil {
+		return err
+	}
+	filePath := os.ExpandEnv(filepath.Join(dao.Path, name+".json"))
+
+	_, err = os.Stat(dao.Path)
+	if err != nil && os.IsNotExist(err) {
+		if err := os.Mkdir(dao.Path, os.ModePerm); err != nil {
+			return err
+		}
+	}
+	fp, err := os.OpenFile(
+		filePath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644,
+	)
+	defer fp.Close()
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Fprintf(fp, "%s\n", string(bz))
+	return nil
+}
+
+func (dao KeyDAO) Read(name string) (types.Store, error) {
+	filePath := dao.Path + string(os.PathSeparator) + name + ".json"
+	f, err := os.OpenFile(filePath, os.O_RDONLY, 0600)
+	defer f.Close()
+	if err != nil {
+		return nil, err
+	}
+	contentByte, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+
+	var keystoreInfo types.KeystoreInfo
+	err = json.Unmarshal(contentByte, &keystoreInfo)
+	if err != nil {
+		return nil, err
+	}
+	return keystoreInfo, nil
+}
+
+func (dao KeyDAO) Delete(name string) error {
+	filePath := dao.Path + string(os.PathSeparator) + name + ".json"
+	err := os.Remove(filePath)
+	return err
+}
--- iservice/cmd/keys/keys.go
+++ iservice/cmd/keys/keys.go
@@ -2,19 +2,14 @@ package keys
 
 import (
 	"os"
-	"os/user"
+	"path/filepath"
 
+	"github.com/irisnet/irishub-sdk-go/crypto"
+	"github.com/irisnet/irishub-sdk-go/types"
 	"github.com/spf13/cobra"
 )
 
-var keysPath = ".iservice"
-
-func init() {
-	u, err := user.Current()
-	if err != nil && u != nil {
-		keysPath = u.HomeDir + string(os.PathSeparator) + ".iservice"
-	}
-}
+var keysPath = os.ExpandEnv(filepath.Join("$HOME", ".iservice"))
 
 func Commands() *cobra.Command {
 	cmd := &cobra.Command{
@@ -31,10 +26,26 @@ func Commands() *cobra.Command {
 
 func addKeyCommand() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "add <name>",
+		Use:   "add [name]",
 		Short: "Create a new key",
 		RunE: func(cmd *cobra.Command, args []string) error {
+			keyDao := NewKeyDAO(keysPath)
+			keyManager, err := crypto.NewKeyManager()
+			if err != nil {
+				return err
+			}
+			keyManager.GetPrivKey()
 
+			var keyInfo types.KeyInfo
+			privateKey, err := keyManager.ExportAsPrivateKey()
+			if err != nil {
+				return err
+			}
+			keyInfo.PrivKey = privateKey
+			err = keyDao.Write(args[0], keyInfo)
+			if err != nil {
+				return err
+			}
 			return nil
 		},
 	}
